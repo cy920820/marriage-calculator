a950f3d8bf54883635ec4170a785b9e73a99dc2b
cy920820 <cuiyang673308817@gmail.com>
2021-12-22 17:03:54 +0800

    adjust project direction

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-function countdown() {
+function statistics() {
   let now = Date.now()
-  let time = '2021-10-01 01:03:14'
-  let future = Date.parse(time.replace(/-/g, '/'))
-  let diff = future - now
+  let time = '2021-10-02 12:00:00'
+  let past = Date.parse(time.replace(/-/g, '/'))
+  let diff = now - past
   d.innerHTML = ~~(diff / 1000 / 60 / 60 / 24)
   h.innerHTML = ~~(diff / 1000 / 60 / 60 % 24)
   m.innerHTML = ~~(diff / 1000 / 60 % 60)
   s.innerHTML = ~~(diff / 1000 % 60)
-  setTimeout(countdown, 1000)
+  setTimeout(statistics, 1000)
 }
 
-countdown()
+statistics()
