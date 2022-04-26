3732097f121d330c5e026f4ec5aa2f7a1bf105c2
Cui Yang <yang.cui@opay-inc.com>
2022-04-26 11:38:04 +0800

    update Start time

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 function statistics() {
   let now = Date.now()
-  let time = '2021-10-02 12:00:00'
-  let past = Date.parse(time.replace(/-/g, '/'))
+  // const marriageDate = '2021-10-02 12:00:00'
+  const getLicenseDate = '2021-05-06 12:00:00'
+  let past = Date.parse(getLicenseDate.replace(/-/g, '/'))
   let diff = now - past
   d.innerHTML = ~~(diff / 1000 / 60 / 60 / 24)
-  h.innerHTML = ~~(diff / 1000 / 60 / 60 % 24)
-  m.innerHTML = ~~(diff / 1000 / 60 % 60)
-  s.innerHTML = ~~(diff / 1000 % 60)
+  h.innerHTML = ~~((diff / 1000 / 60 / 60) % 24)
+  m.innerHTML = ~~((diff / 1000 / 60) % 60)
+  s.innerHTML = ~~((diff / 1000) % 60)
   setTimeout(statistics, 1000)
 }
 
